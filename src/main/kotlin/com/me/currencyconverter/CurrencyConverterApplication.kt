5318672a5cea0e2ae96af640e38804e6cb4bfba9
carlasanches <carlasanches8@gmail.com>
2023-12-14 17:22:09 -0300

    Create Spring Boot project
    Create Spring Boot project with Spring Web and Validation support.

---
+++ src/main/kotlin/com/me/currencyconverter/CurrencyConverterApplication.kt
@@ -0,0 +1,11 @@
+package com.me.currencyconverter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CurrencyConverterApplication
+
+fun main(args: Array<String>) {
+	runApplication<CurrencyConverterApplication>(*args)
+}
