10cf517c081e12de0a0a25ae8cd262d3b77e544a
carlasanches <carlasanches8@gmail.com>
2023-12-14 22:41:43 -0300

    Update Spring Boot project
    Update Spring Boot project with missing dependencies: Spring Data JPA, Flyway Migration, and H2 Database.

--- src/main/kotlin/com/me/currency/converter/CurrencyConverterApplication.kt
+++ src/main/kotlin/com/me/currency/converter/CurrencyConverterApplication.kt
@@ -1,4 +1,4 @@
-package com.me.currencyconverter
+package com.me.currency.converter
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
