a2a71429c49cb7b8761080d5ebd93f815fca24d7
carlasanches <carlasanches8@gmail.com>
2023-12-17 08:37:27 -0300

    Setup log4j2 and remove StackSpot workflows
    This commit sets log4j2 for observability and removes StackSpot workflows that were accidentally generated in this repository.

--- src/main/kotlin/com/me/currency/converter/controller/ConversionRateController.kt
+++ src/main/kotlin/com/me/currency/converter/controller/ConversionRateController.kt
@@ -7,14 +7,7 @@ import com.me.currency.converter.entity.ConversionRate
 import com.me.currency.converter.service.impl.ConversionRateService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/conversion_rates")
